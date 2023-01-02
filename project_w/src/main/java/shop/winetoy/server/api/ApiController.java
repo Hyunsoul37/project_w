8a62ef766d4dc15b49e3b97174aa436ed814ae20
Hyunsoul37 <soulhyun37@gmail.com>
2023-01-02 20:59:33 +0900

    [ADD] time api 추가
    [ GET ] Method
    uri : /api/time

---
+++ project_w/src/main/java/shop/winetoy/server/api/ApiController.java
@@ -0,0 +1,27 @@
+package shop.winetoy.server.api;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api")
+public class ApiController {
+	
+	@RequestMapping(value = "/time", method = RequestMethod.GET)
+	@ResponseBody
+	private String showTime() {
+		LocalTime now = LocalTime.now();
+        // 포맷 정의하기
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH시 mm분 ss초");
+        // 포맷 적용하기
+        String formatedNow = now.format(formatter);
+        
+        return formatedNow;
+	}
+	
+}
