d660369c3577eebdd34da58f6e3866ca782b980f
Hyunsoul37 <soulhyun37@gmail.com>
2023-01-06 02:50:15 +0900

    [ADD] DB연동 및 회원가입(/api/join) 구현
    /api/join

--- project_w/src/main/java/shop/winetoy/server/api/ApiController.java
+++ project_w/src/main/java/shop/winetoy/server/api/ApiController.java
@@ -2,83 +2,66 @@ package shop.winetoy.server.api;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import shop.winetoy.server.member.MemberInfo;
+import shop.winetoy.server.member.entity.MemberInfoVO;
+import shop.winetoy.server.member.service.MemberInfoService;
+
+
 
 @Controller
 @RequestMapping("/api")
 public class ApiController {
 	
+	@Autowired
+	MemberInfoService memberInfoService;
+	
 	// 서버 시간 조회
 	@RequestMapping(value = "/time", method = RequestMethod.GET)
 	@ResponseBody
-	public Time time() {
+	public Map<String,String> time() {
 		LocalDateTime now = LocalDateTime.now();
 		String format = now.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초"));
 
-		Time _time = new Time();
-		_time.setTime(format);
+		HashMap<String,String> map = new HashMap<String,String>();
 		
-		return _time;
+		map.put("time", format);
+		
+		
+		return map;
 	}
 	
 	@RequestMapping(value = "/test", method = RequestMethod.POST)
 	@ResponseBody
-	public Data postData(@RequestBody Data data){
-		System.out.println("/test , RequestMethod.POST");
+	public Map<String,String> postData(@RequestBody Map<String,String> data){
 		return data ;
 	}
 	
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	@ResponseBody
-	public Data gettData(){
-		Data data = new Data();
+	public Map<String,String> getData(){
+		Map<String,String> data = new HashMap<String,String>();
 		System.out.println("/test ,  RequestMethod.GET");
-		data.setData("RequestMethod.GET");
+		data.put("data","RequestMethod.GET");
 		
 		return data;
 	}
 	
+	
 	@RequestMapping(value = "/join", method = RequestMethod.POST)
 	@ResponseBody
-	public MemberInfo join(@RequestBody MemberInfo info) {
-		MemberInfo memberInfo = new MemberInfo();
-	
-		memberInfo = info;
-		memberInfo.setPid(20211224L);
-		
-		return memberInfo;
-	}
-	
-	static class Data{
-		private String data;
-
-		public String getData() {
-			return data;
-		}
-
-		public void setData(String data) {
-			this.data = data;
-		}
-		
-	}
-	
-	static class Time{
-        private String time;
-
-		public String getTime() {
-			return time;
-		}
-
-		public void setTime(String time) {
-			this.time = time;
-		}
-	
+	public int join(@RequestBody MemberInfoVO info) {
+		System.out.println(info.toString());
+		int result = memberInfoService.join(info);
+		System.out.println(result);
+		return result;
 	}
 }
---
+++ project_w/src/main/java/shop/winetoy/server/member/service/MemberInfoService.java
@@ -0,0 +1,7 @@
+package shop.winetoy.server.member.service;
+
+import shop.winetoy.server.member.entity.MemberInfoVO;
+
+public interface MemberInfoService {
+	int join(MemberInfoVO memberInfo);
+}
