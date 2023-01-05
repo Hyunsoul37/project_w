a1546dd2497d61b739ed7b90775dfe37d2fc5d92
Hyunsoul37 <soulhyun37@gmail.com>
2023-01-06 00:58:54 +0900

    [ADD] CORS Filter, MemberInfo 추가

---
+++ project_w/src/main/java/shop/winetoy/server/CORSFilter.java
@@ -0,0 +1,42 @@
+package shop.winetoy.server;
+
+//cross domain 설정 
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CORSFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+
+//		response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+		response.setHeader("Access-Control-Allow-Origin", "http://winetoy.shop");
+
+		response.setHeader("Access-Control-Allow-Method", "POST,GET,OPTIONS,DELETE, PUT");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers",
+				"Content-Type,x-requested-with,Authorization,Axxess-Control-Allow-Origin");
+
+		chain.doFilter(req, res);
+	}
+
+	@Override
+	public void destroy() {
+	}
+}
