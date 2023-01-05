7187601be8b000dd9452e682786411de4eb16c3d
sunmin <dlatjsals123@naver.com>
2023-01-05 22:29:39 +0900

    [ADD] 회원가입 폼 추가 및 GET,POST Test

--- project_w/src/main/frontend/src/App.js
+++ project_w/src/main/frontend/src/App.js
@@ -1,19 +1,59 @@
 import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import Signupform from './components/Signupform';
+import styled from'./App.module.css';
+
 
 function App() {
-   const [hello, setHello] = useState('')
+//    const [time, setTime] = useState('');
+//    const [data, setData] = useState('');
+//    const [responsePostData, setResponsePostData] = useState('');
 
-    useEffect(() => {
-        axios.get('api/time')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-    }, []);
+//      const gettime = async()=>{
+//         try{
+//             const res = await fetch("/api/time");
+//             //const res = await fetch("/api/test");
+//             if(!res.ok){
+//                 throw new Error("Request failed!");
+//             }
+//             const resData = await res.json();
+//             setTime(resData.time);
+//             //setData(data.data);
+//         }
+//         catch(err){
+//             console.log(err);
+//         } 
+//      }
+
+//      const postData = async()=>{
+//         try{
+//             const res = await fetch("/api/test",{
+//                 method:'POST',
+//                 body:JSON.stringify({data : '이호현 귀요미 귀귀ㅜ기ㅜ기ㅜ기ㅜ귀요미'}),
+//                 headers:{
+                    
+//                     "Content-Type": "application/json",
+//                 }
+//             });
+//             const responseData = await res.json();
+//             setResponsePostData(responseData.data);
+//         }
+//         catch(err){
+//             console.log(err);
+//         }
+//      }
+
+//      useEffect(() => {
+//         gettime();
+//         postData();
+//     }, []);
 
     return (
-        <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
+        <div className={styled.App}>
+            {/* <div>백엔드에서 가져온 getData : {time? time : data}</div>
+            <div>백엔드에서 가져온 postData : {responsePostData? responsePostData: ""}</div>            */}
+            <Signupform />
         </div>
+        
     );
 }
 
