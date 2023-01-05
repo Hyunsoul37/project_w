26be2c3b5bd5978945209bcccdc8c27922ef04c9
Hyunsoul37 <soulhyun37@gmail.com>
2023-01-06 01:00:04 +0900

    [FIX] artifactId demo -> project_w, apiController 수정

--- project_w/src/main/java/shop/winetoy/server/api/ApiController.java
+++ project_w/src/main/java/shop/winetoy/server/api/ApiController.java
@@ -1,29 +1,84 @@
 package shop.winetoy.server.api;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import shop.winetoy.server.member.MemberInfo;
+
 @Controller
 @RequestMapping("/api")
-@CrossOrigin("*")
 public class ApiController {
 	
+	// 서버 시간 조회
 	@RequestMapping(value = "/time", method = RequestMethod.GET)
 	@ResponseBody
-	private String showTime() {
-		LocalTime now = LocalTime.now();
-        // 포맷 정의하기
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH시 mm분 ss초");
-        // 포맷 적용하기
-        String formatedNow = now.format(formatter);
-        
-        return formatedNow;
+	public Time time() {
+		LocalDateTime now = LocalDateTime.now();
+		String format = now.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초"));
+
+		Time _time = new Time();
+		_time.setTime(format);
+		
+		return _time;
 	}
 	
+	@RequestMapping(value = "/test", method = RequestMethod.POST)
+	@ResponseBody
+	public Data postData(@RequestBody Data data){
+		System.out.println("/test , RequestMethod.POST");
+		return data ;
+	}
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	@ResponseBody
+	public Data gettData(){
+		Data data = new Data();
+		System.out.println("/test ,  RequestMethod.GET");
+		data.setData("RequestMethod.GET");
+		
+		return data;
+	}
+	
+	@RequestMapping(value = "/join", method = RequestMethod.POST)
+	@ResponseBody
+	public MemberInfo join(@RequestBody MemberInfo info) {
+		MemberInfo memberInfo = new MemberInfo();
+	
+		memberInfo = info;
+		memberInfo.setPid(20211224L);
+		
+		return memberInfo;
+	}
+	
+	static class Data{
+		private String data;
+
+		public String getData() {
+			return data;
+		}
+
+		public void setData(String data) {
+			this.data = data;
+		}
+		
+	}
+	
+	static class Time{
+        private String time;
+
+		public String getTime() {
+			return time;
+		}
+
+		public void setTime(String time) {
+			this.time = time;
+		}
+	
+	}
 }
