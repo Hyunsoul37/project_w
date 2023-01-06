8ed048a9e95d48192253fe4cbab9286d795e1fd6
Hyunsoul37 <soulhyun37@gmail.com>
2023-01-06 14:42:43 +0900

    [ADD] Member List 확인 패킷 추가
    /api/memberList

--- project_w/src/main/java/shop/winetoy/server/member/service/MemberInfoServiceImp.java
+++ project_w/src/main/java/shop/winetoy/server/member/service/MemberInfoServiceImp.java
@@ -1,10 +1,12 @@
 package shop.winetoy.server.member.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import shop.winetoy.server.member.dao.MemberInfoDao;
-import shop.winetoy.server.member.entity.MemberInfoVO;
+import shop.winetoy.server.member.entity.MemberInfoDto;
 
 @Service
 public class MemberInfoServiceImp implements MemberInfoService{
@@ -12,9 +14,21 @@ public class MemberInfoServiceImp implements MemberInfoService{
 	@Autowired
 	MemberInfoDao memberInfoDao;
 	
+	/**
+	 * 회원 가입
+	 */
 	@Override
-	public int join(MemberInfoVO memberInfo) {
+	public int join(MemberInfoDto memberInfo) {
 		int result = memberInfoDao.join(memberInfo);
+		return result;
+	}
+
+	/**
+	 * 회원 리스트 조회
+	 */
+	@Override
+	public List<MemberInfoDto> memberList() {
+		List<MemberInfoDto> result = memberInfoDao.memberList();
 		return result;
 	}
 
