665b681cc35fd50ca6293953e4df4f0d824d4b54
sunmin <dlatjsals123@naver.com>
2023-01-06 18:26:46 +0900

    [ADD] 회원가입, 로그인 Router 추가

--- project_w/src/main/frontend/src/App.js
+++ project_w/src/main/frontend/src/App.js
@@ -1,60 +1,21 @@
-import React, {useEffect, useState} from 'react';
-import Signupform from './components/Signupform';
-import styled from'./App.module.css';
-
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Signupform from './components/signup/Signupform';
+import styled from './App.module.css';
+import Home from './components/Home';
+import Login from './components/login/Login';
 
 function App() {
-//    const [time, setTime] = useState('');
-//    const [data, setData] = useState('');
-//    const [responsePostData, setResponsePostData] = useState('');
-
-//      const gettime = async()=>{
-//         try{
-//             const res = await fetch("/api/time");
-//             //const res = await fetch("/api/test");
-//             if(!res.ok){
-//                 throw new Error("Request failed!");
-//             }
-//             const resData = await res.json();
-//             setTime(resData.time);
-//             //setData(data.data);
-//         }
-//         catch(err){
-//             console.log(err);
-//         } 
-//      }
-
-//      const postData = async()=>{
-//         try{
-//             const res = await fetch("/api/test",{
-//                 method:'POST',
-//                 body:JSON.stringify({data : '이호현 귀요미 귀귀ㅜ기ㅜ기ㅜ기ㅜ귀요미'}),
-//                 headers:{
-                    
-//                     "Content-Type": "application/json",
-//                 }
-//             });
-//             const responseData = await res.json();
-//             setResponsePostData(responseData.data);
-//         }
-//         catch(err){
-//             console.log(err);
-//         }
-//      }
-
-//      useEffect(() => {
-//         gettime();
-//         postData();
-//     }, []);
-
-    return (
-        <div className={styled.App}>
-            {/* <div>백엔드에서 가져온 getData : {time? time : data}</div>
-            <div>백엔드에서 가져온 postData : {responsePostData? responsePostData: ""}</div>            */}
-            <Signupform />
-        </div>
-        
-    );
+  return (
+    <div className={styled.App}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/join" element={<Signupform />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
-
 export default App;
