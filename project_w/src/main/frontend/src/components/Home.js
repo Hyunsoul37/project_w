6b42e65a77e242540015afe95fe6b640cc4178e6
sunmin <dlatjsals123@naver.com>
2023-01-12 18:32:51 +0900

    [ADD] Token 유효기간, DB 초기화 버튼(테스트용) 추가

--- project_w/src/main/frontend/src/components/Home.js
+++ project_w/src/main/frontend/src/components/Home.js
@@ -1,28 +1,40 @@
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { logout } from '../slice/userSlice';
+import { logout, DataClear } from '../slice/userSlice';
 
 const Home = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  const userData = user.userData ? user.userData.data : null;
   const LoginBtnHandler = () => {
     navigate('/login');
   };
   const LogOutHandler = () => {
     dispatch(logout());
   };
-  const user = useSelector(state => state.user);
+
+  const DataCleatHandler = () => {
+    dispatch(DataClear());
+  };
+
+  useEffect(() => {
+    console.log('랜더!');
+  }, []);
+
   return (
     <div>
       <p>Home Page</p>
       {user.isLoggedIn ? (
         <>
-          <div> Hi!!</div>
+          <div> Hi!! {userData.name}</div>
           <button onClick={LogOutHandler}>Logout</button>
         </>
       ) : (
         <button onClick={LoginBtnHandler}>Login</button>
       )}
+      <button onClick={DataCleatHandler}>Data All Clear</button>
     </div>
   );
 };
