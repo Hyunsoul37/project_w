aff6fdac021ae9b6b0e28ea12df9755cd6948c83
sunmin <dlatjsals123@naver.com>
2023-01-24 18:59:59 +0900

    [Fix] BestReview에 Modal창 연결

--- project_w/src/main/frontend/src/components/ui/Card.js
+++ project_w/src/main/frontend/src/components/ui/Card.js
@@ -1,6 +1,14 @@
 import styled from './Card.module.css';
 
 const Card = props => {
-  return <div className={styled.Card}>{props.children}</div>;
+  return (
+    <div
+      id={props.id}
+      className={styled.Card}
+      onClick={e => props.onClick(e)}
+    >
+      {props.children}
+    </div>
+  );
 };
 export default Card;
