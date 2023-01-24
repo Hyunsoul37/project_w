0f70fdc89ace98bf2f900b54ee21be55a1b6c98a
sunmin <dlatjsals123@naver.com>
2023-01-24 17:07:44 +0900

    [ADD] BestReview, Modal창 추가

--- project_w/src/main/frontend/src/components/home/Header.js
+++ project_w/src/main/frontend/src/components/home/Header.js
@@ -1,6 +1,42 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { logout, DataClear } from '../../slice/userSlice';
 import styled from './Header.module.css';
 
 const Header = () => {
-  return <div className={styled.Header}>Header!!!!!!!!!!!</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.user);
+  const userData = user.userData ? user.userData.data : null;
+
+  const LoginBtnHandler = () => {
+    navigate('/login');
+  };
+  const LogOutHandler = () => {
+    dispatch(logout());
+  };
+
+  const DataClearHandler = () => {
+    dispatch(DataClear());
+  };
+
+  useEffect(() => {
+    // 로그인 유지 로직 작성
+  }, []);
+
+  return (
+    <div className={styled.Header}>
+      {user.isLoggedIn ? (
+        <>
+          <div> Hi!! {userData.name}</div>
+          <button onClick={LogOutHandler}>Logout</button>
+        </>
+      ) : (
+        <button onClick={LoginBtnHandler}>Login</button>
+      )}
+      {/* <button onClick={DataClearHandler}>Data Clear</button> */}
+    </div>
+  );
 };
 export default Header;
--- project_w/src/main/frontend/src/components/home/Home.js
+++ project_w/src/main/frontend/src/components/home/Home.js
@@ -1,43 +1,12 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { logout, DataClear } from '../../slice/userSlice';
 import Banner from './Banner';
+import BestReview from './BestReview';
 import styled from './Home.module.css';
 
 const Home = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const user = useSelector(state => state.user);
-  const userData = user.userData ? user.userData.data : null;
-
-  const LoginBtnHandler = () => {
-    navigate('/login');
-  };
-  const LogOutHandler = () => {
-    dispatch(logout());
-  };
-
-  const DataClearHandler = () => {
-    dispatch(DataClear());
-  };
-
-  useEffect(() => {
-    // 로그인 유지 로직 작성
-  }, []);
-
   return (
     <div className={styled.Home}>
       <Banner />
-      {user.isLoggedIn ? (
-        <>
-          <div> Hi!! {userData.name}</div>
-          <button onClick={LogOutHandler}>Logout</button>
-        </>
-      ) : (
-        <button onClick={LoginBtnHandler}>Login</button>
-      )}
-      <button onClick={DataClearHandler}>Data All Clear</button>
+      <BestReview />
     </div>
   );
 };
