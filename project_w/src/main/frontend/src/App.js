db01c2852917f31ad63bc07a2354066014f558d4
hyooo11 <tlsgywls1107@naver.com>
2023-02-03 21:19:51 +0900

    [ADD] 메인 랭킹상품 영역 추가

--- project_w/src/main/frontend/src/App.js
+++ project_w/src/main/frontend/src/App.js
@@ -5,10 +5,22 @@ import styled from './App.module.css';
 import Home from './components/home/Home';
 import Login from './components/login/Login';
 import Header from './components/home/Header';
+// 글로벌 스타일 만들기 위한 createGlobalStyle
+import { createGlobalStyle } from "styled-components";
+// 스타일 초기화를 위한 reset
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+    ${reset}
+    body {
+        background-color: #fff;
+    }
+`;
 
 function App() {
   return (
     <div className={styled.App}>
+      <GlobalStyles />
       <Header />
       <Routes>
         <Route
@@ -24,6 +36,7 @@ function App() {
           element={<Signupform />}
         />
       </Routes>
+
     </div>
   );
 }
--- project_w/src/main/frontend/src/components/home/RankProd.js
+++ project_w/src/main/frontend/src/components/home/RankProd.js
@@ -1,13 +1,98 @@
-// const rackContent = [
-//     tab:'레드',
-//     cotent : ''
-// ]
+import React, { useState } from "react";
 
-const RankProd = () =>{
-    return(
+const RankProd = () => {
+    const [currentTab, clickTab] = useState(0);
+    const menuArr = [
+        {
+            name: '레드',
+            content1: '레드 랭킹1 사진 넣기',
+            content2: '레드 랭킹2 사진 넣기',
+            content3: '레드 랭킹3 사진 넣기',
+            content4: '레드 랭킹4 사진 넣기',
+            content5: '레드 랭킹5 사진 넣기'
+        },
+        {
+            name: '화이트',
+            content1: '화이트 랭킹1 사진 넣기',
+            content2: '화이트 랭킹2 사진 넣기',
+            content3: '화이트 랭킹3 사진 넣기',
+            content4: '화이트 랭킹4 사진 넣기',
+            content5: '화이트 랭킹5 사진 넣기'
+        },
+        {
+            name: '스파클링',
+            content1: '스파클링 랭킹1 사진 넣기',
+            content2: '스파클링 랭킹2 사진 넣기',
+            content3: '스파클링 랭킹3 사진 넣기',
+            content4: '스파클링 랭킹4 사진 넣기',
+            content5: '스파클링 랭킹5 사진 넣기'
+        },
+        {
+            name: '주정강화',
+            content1: '주정강화 랭킹1 사진 넣기',
+            content2: '주정강화 랭킹2 사진 넣기',
+            content3: '주정강화 랭킹3 사진 넣기',
+            content4: '주정강화 랭킹4 사진 넣기',
+            content5: '주정강화 랭킹5 사진 넣기'
+        },
+    ];
+    const selectMenuHandler = (index) => {
+        clickTab(index);
+    };
+    return (
         <>
-            <span>Ranking Product</span>
-            <h2>금주 와토 랭킹상품</h2>
+            <div>
+                <div>
+                    <ul style={{ display: 'flex', justifyContent: 'center' }}>
+                        {menuArr.map((el, index) => (
+                            <li
+                                className={index === currentTab ? "submenu focused" : "submenu"}
+                                onClick={() => selectMenuHandler(index)}
+                                style={{ width: '100px', margin: '0 5px', cursor: 'pointer' }}
+                            >
+                                {el.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="tab_wrap" style={{ width: '1000px', margin: '0 auto', display: 'flex', flexWrap: 'wrap' }}>
+                    <div style={{ width: '500px', height: '500px', backgroundColor: 'red' }} >
+                        <p>{menuArr[currentTab].content1}</p>
+                        <div className="text_area">
+                            <span>Ranking 01</span>
+                            <p>블라블라숑 레드01</p>
+                            <p className="price">12,900</p>
+
+                        </div>
+                    </div>
+                    <ul style={{ width: '500px', display: 'flex', flexWrap: 'wrap' }}>
+                        <li style={{ width: '250px', height: '250px', backgroundColor: 'tomato' }} >
+                            <p>{menuArr[currentTab].content2}</p>
+                            <span>Ranking 02</span>
+                            <p>블라블라숑 레드02</p>
+                            <p className="price">12,900</p>
+                        </li>
+                        <li style={{ width: '250px', height: '250px', backgroundColor: 'yellow' }}>
+                            <p>{menuArr[currentTab].content3}</p>
+                            <span>Ranking 03</span>
+                            <p>블라블라숑 레드03</p>
+                            <p className="price">12,900</p>
+                        </li>
+                        <li style={{ width: '250px', height: '250px', backgroundColor: 'skyblue' }}>
+                            <p>{menuArr[currentTab].content4}</p>
+                            <span>Ranking 04</span>
+                            <p>블라블라숑 레드04</p>
+                            <p className="price">12,900</p>
+                        </li>
+                        <li style={{ width: '250px', height: '250px', backgroundColor: 'purple' }}>
+                            <p>{menuArr[currentTab].content5}</p>
+                            <span>Ranking 05</span>
+                            <p>블라블라숑 레드05</p>
+                            <p className="price">12,900</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </>
     );
 }
