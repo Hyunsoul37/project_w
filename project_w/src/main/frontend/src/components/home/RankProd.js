3927aa9d8bf05e2b64eca5fb36a3a8af826a211f
hyooojin <tlsgywls1107@naver.com>
2023-02-03 16:47:58 +0900

    [ADD]메인 Ranking product 작업중

---
+++ project_w/src/main/frontend/src/components/home/RankProd.js
@@ -0,0 +1,15 @@
+// const rackContent = [
+//     tab:'레드',
+//     cotent : ''
+// ]
+
+const RankProd = () =>{
+    return(
+        <>
+            <span>Ranking Product</span>
+            <h2>금주 와토 랭킹상품</h2>
+        </>
+    );
+}
+
+export default RankProd;
