05582b60c4cb0a0336cf364f5e78955e4c22c923
sunmin <dlatjsals123@naver.com>
2023-02-07 19:29:44 +0900

    [FIX] 헤더 공통 영역으로 이동

--- project_w/src/main/frontend/src/pages/index.js
+++ project_w/src/main/frontend/src/pages/index.js
@@ -2,13 +2,11 @@ import Seo from '../util/Seo';
 import Banner from '../components/home/Banner';
 import BestReview from '../components/home/BestReview';
 import SelectorArea from '../components/home/SelectorArea';
-import Header from '../components/home/Header';
 
 const Home = () => {
   return (
     <div>
       <Seo title="Home" />
-      <Header />
       <Banner />
       <SelectorArea />
       <BestReview />
