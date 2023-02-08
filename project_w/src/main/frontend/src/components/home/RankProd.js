827985f31b8d23f604d138665ab9a1c1ecf5a3ba
hyooo11 <tlsgywls1107@naver.com>
2023-02-08 20:57:42 +0900

    [ADD] css 세팅

--- project_w/src/main/frontend/src/components/home/RankProd.js
+++ project_w/src/main/frontend/src/components/home/RankProd.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from './RankProd.module.css'
 const menuArr = [
   {
     name: '레드',
@@ -40,93 +41,64 @@ const RankProd = () => {
     clickTab(index);
   };
   return (
-    <>
-      <div>
-        <div>
-          <ul style={{ display: 'flex', justifyContent: 'center' }}>
-            {menuArr.map((el, index) => (
-              <li
-                className={index === currentTab ? 'submenu focused' : 'submenu'}
-                onClick={() => selectMenuHandler(index)}
-                style={{ width: '100px', margin: '0 5px', cursor: 'pointer' }}
-              >
-                {el.name}
-              </li>
-            ))}
-          </ul>
+    <section>
+      <div className='maxframe'>
+        <div className='sub_tit__'>
+          <h4>카테고리별 랭킹</h4>
+          <span>CATECORY’S RANKING</span>
         </div>
-        <div
-          className="tab_wrap"
-          style={{
-            width: '1000px',
-            margin: '0 auto',
-            display: 'flex',
-            flexWrap: 'wrap',
-          }}
-        >
-          <div
-            style={{ width: '500px', height: '500px', backgroundColor: 'red' }}
-          >
-            <p>{menuArr[currentTab].content1}</p>
-            <div className="text_area">
-              <span>Ranking 01</span>
-              <p>블라블라숑 레드01</p>
-              <p className="price">12,900</p>
-            </div>
+        <div>
+          <div className={styled.tablist}>
+            <ul>
+              {menuArr.map((el, index) => (
+                <li
+                  className={index === currentTab ? 'submenu focused' : 'submenu'}
+                  onClick={() => selectMenuHandler(index)}
+                >
+                  {el.name}
+                </li>
+              ))}
+            </ul>
           </div>
-          <ul style={{ width: '500px', display: 'flex', flexWrap: 'wrap' }}>
-            <li
-              style={{
-                width: '250px',
-                height: '250px',
-                backgroundColor: 'tomato',
-              }}
-            >
-              <p>{menuArr[currentTab].content2}</p>
-              <span>Ranking 02</span>
-              <p>블라블라숑 레드02</p>
-              <p className="price">12,900</p>
-            </li>
-            <li
-              style={{
-                width: '250px',
-                height: '250px',
-                backgroundColor: 'yellow',
-              }}
-            >
-              <p>{menuArr[currentTab].content3}</p>
-              <span>Ranking 03</span>
-              <p>블라블라숑 레드03</p>
-              <p className="price">12,900</p>
-            </li>
-            <li
-              style={{
-                width: '250px',
-                height: '250px',
-                backgroundColor: 'skyblue',
-              }}
-            >
-              <p>{menuArr[currentTab].content4}</p>
-              <span>Ranking 04</span>
-              <p>블라블라숑 레드04</p>
-              <p className="price">12,900</p>
-            </li>
-            <li
-              style={{
-                width: '250px',
-                height: '250px',
-                backgroundColor: 'purple',
-              }}
-            >
-              <p>{menuArr[currentTab].content5}</p>
-              <span>Ranking 05</span>
-              <p>블라블라숑 레드05</p>
-              <p className="price">12,900</p>
-            </li>
-          </ul>
+          <div className={styled.tabmenu_wrap}>
+            <div className={styled.rank01}>
+              <p>{menuArr[currentTab].content1}</p>
+              <div className="text_area">
+                <span>Rank01</span>
+                <p>블라블라숑 레드01</p>
+                <p className="price">12,900</p>
+              </div>
+            </div>
+            <ul className={styled.diffrank}>
+              <li>
+                <p>{menuArr[currentTab].content2}</p>
+                <span>Ranking 02</span>
+                <p>블라블라숑 레드02</p>
+                <p className="price">12,900</p>
+              </li>
+              <li>
+                <p>{menuArr[currentTab].content3}</p>
+                <span>Ranking 03</span>
+                <p>블라블라숑 레드03</p>
+                <p className="price">12,900</p>
+              </li>
+              <li>
+                <p>{menuArr[currentTab].content4}</p>
+                <span>Ranking 04</span>
+                <p>블라블라숑 레드04</p>
+                <p className="price">12,900</p>
+              </li>
+              <li>
+                <p>{menuArr[currentTab].content5}</p>
+                <span>Ranking 05</span>
+                <p>블라블라숑 레드05</p>
+                <p className="price">12,900</p>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </>
+    </section>
   );
 };
 
