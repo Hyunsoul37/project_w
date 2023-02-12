2fba2f6c766ea6f4ee613d56d5d672716f250089
sunmin <dlatjsals123@naver.com>
2023-02-12 18:18:47 +0900

    [FIX]Selector Slider option 수정

--- project_w/src/main/frontend/src/components/home/SelectorSlider.js
+++ project_w/src/main/frontend/src/components/home/SelectorSlider.js
@@ -2,36 +2,43 @@ import Slider from 'react-slick';
 import styled from './Selector.module.css';
 
 const SelectorSlider = () => {
-   const settings = {
-      infinite: true,
-      slidesToShow: 1,
-      variableWidth: true,
-      pauseOnFocus: false,
-      pauseOnHover: false,
-      centerMode: true,
-      touchRatio: 0,
-      autoplay: true,
-      autoplaySpeed: 500,
-   };
-   const flowImgs = []
-   for (let idx = 0; idx <= 14; idx++) {
-      flowImgs.push(`/images/main_flow${idx}.png`);
-   }
+  const settings = {
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    centerMode: true,
+    touchRatio: 0,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    swipeToSlide: false,
+    draggable: false,
+    arrows: false,
+    cssEase: 'linear',
+    //  easing: 'linear',
+    //  cssEase: 'cubic-bezier(0.250, 0.250, 0.750, 0.750)',
+  };
+  const flowImgs = [];
+  for (let idx = 0; idx <= 14; idx++) {
+    flowImgs.push(`/images/main_flow${idx}.png`);
+  }
 
-   return (
-      <div className={styled.h25}>
-         <Slider {...settings}>
-            {flowImgs.map((flowImgs, idx) => (
-               <img
-                  key={`flowimg${idx}`}
-                  src={flowImgs}
-                  alt={`flowimg${idx}`}
-                  className={styled.h25}
-               />
-            ))}
-         </Slider>
-      </div>
-   );
-}
+  return (
+    <div className={styled.h25}>
+      <Slider {...settings}>
+        {flowImgs.map((flowImgs, idx) => (
+          <img
+            key={`flowimg${idx}`}
+            src={flowImgs}
+            alt={`flowimg${idx}`}
+            className={styled.h25}
+          />
+        ))}
+      </Slider>
+    </div>
+  );
+};
 
-export default SelectorSlider; 
+export default SelectorSlider;
