6539d93c79614c17ae3140f002b9c673f938e990
sunmin <dlatjsals123@naver.com>
2023-02-13 22:25:59 +0900

    [FIX] 로그인 & 회원가입 연결

--- project_w/src/main/frontend/src/components/ui/Form.js
+++ project_w/src/main/frontend/src/components/ui/Form.js
@@ -1,13 +1,18 @@
 import styled from './Form.module.css';
+import { useRouter } from 'next/router';
 
-const Form = props => {
+const Form = (props) => {
+  const router = useRouter();
   return (
-    <form
-      className={styled.form}
-      onSubmit={props.onsubmit}
-    >
-      {props.children}
-    </form>
+    <div className={styled.formWrapper}>
+      <form
+        className={styled.form}
+        onSubmit={props.onsubmit}
+      >
+        {props.children}
+      </form>
+      <button onClick={() => router.push('/join')}>SignUp</button>
+    </div>
   );
 };
 export default Form;
