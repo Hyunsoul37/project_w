866ba98fd0fb90aa83baab2155a5d5e03d337d09
Hyunsoul37 <soulhyun37@gmail.com>
2023-02-20 02:57:21 +0900

    [ADD] wine db 업로드 코드 및 wine type에 따른 검색 추가

--- project_w/src/main/java/shop/winetoy/server/wine/dao/WineDao.java
+++ project_w/src/main/java/shop/winetoy/server/wine/dao/WineDao.java
@@ -1,8 +1,18 @@
 package shop.winetoy.server.wine.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import shop.winetoy.server.wine.entity.WineDto;
+import shop.winetoy.server.wine.entity.WineInfoDto;
 
 @Mapper
 public interface WineDao {
 
+	int registerWine(WineDto wine);
+
+	WineDto getWineDetail(int pid);
+
+	List<WineInfoDto> searchWine(int type, int listCount, int offset);
 }
