b7c201e0be7b0d941180a1fa67890a6a7260be26
hyooo11 <tlsgywls1107@naver.com>
2023-02-24 20:26:44 +0900

    [ADD]랭킹 당도,산미 아이콘 완료(일까 새로운 시작일까)

--- project_w/src/main/frontend/src/components/home/RankProd.js
+++ project_w/src/main/frontend/src/components/home/RankProd.js
@@ -14,35 +14,35 @@ const menuArr = [
       pdname: '블라블라숑 레드01',
       pdprice: '12,900',
       pdtsweet: 3,
-      pdacid: '5',
+      pdacid: 5,
     },
     content2: {
       pdimg: '/images/main_flow5.png',
       pdname: '블라블라숑 레드02',
       pdprice: '33,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 4,
     },
     content3: {
       pdimg: '/images/main_flow4.png',
       pdname: '블라블라숑 레드03',
       pdprice: '12,900',
       pdtsweet: 1,
-      pdacid: '',
+      pdacid: 4,
     },
     content4: {
       pdimg: '/images/main_flow3.png',
       pdname: '블라블라숑 레드04',
       pdprice: '12,900',
       pdtsweet: 4,
-      pdacid: '',
+      pdacid: 3,
     },
     content5: {
       pdimg: '/images/main_flow2.png',
       pdname: '블라블라숑 레드05',
       pdprice: '12,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 1,
     },
   },
   {
@@ -52,35 +52,35 @@ const menuArr = [
       pdname: '블라블라숑 화이트01',
       pdprice: '42,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 3,
     },
     content2: {
       pdimg: '/images/main_flow2.png',
       pdname: '블라블라숑 화이트02',
       pdprice: '42,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 4,
     },
     content3: {
       pdimg: '/images/main_flow3.png',
       pdname: '블라블라숑 화이트03',
       pdprice: '42,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 1,
     },
     content4: {
       pdimg: '/images/main_flow4.png',
       pdname: '블라블라숑 화이트04',
       pdprice: '42,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 1,
     },
     content5: {
       pdimg: '/images/main_flow5.png',
       pdname: '블라블라숑 화이트05',
       pdprice: '42,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 4,
     },
   },
   {
@@ -90,35 +90,35 @@ const menuArr = [
       pdname: '블라블라숑 스파클링1',
       pdprice: '32,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 2,
     },
     content2: {
       pdimg: '/images/main_flow4.png',
       pdname: '블라블라숑 스파클링2',
       pdprice: '32,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 1,
     },
     content3: {
       pdimg: '/images/main_flow3.png',
       pdname: '블라블라숑 스파클링3',
       pdprice: '32,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 4,
     },
     content4: {
       pdimg: '/images/main_flow2.png',
       pdname: '블라블라숑 스파클링4',
       pdprice: '32,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 5,
     },
     content5: {
       pdimg: '/images/main_flow1.png',
       pdname: '블라블라숑 스파클링5',
       pdprice: '32,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 3,
     },
   },
   {
@@ -128,35 +128,35 @@ const menuArr = [
       pdname: '블라블라숑 주정강화1',
       pdprice: '32,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 1,
     },
     content2: {
       pdimg: '/images/main_flow2.png',
       pdname: '블라블라숑 주정강화2',
       pdprice: '32,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 4,
     },
     content3: {
       pdimg: '/images/main_flow3.png',
       pdname: '블라블라숑 주정강화3',
       pdprice: '32,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 3,
     },
     content4: {
       pdimg: '/images/main_flow4.png',
       pdname: '블라블라숑 주정강화4',
       pdprice: '32,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 1,
     },
     content5: {
       pdimg: '/images/main_flow5.png',
       pdname: '블라블라숑 주정강화5',
       pdprice: '32,900',
       pdtsweet: 2,
-      pdacid: '',
+      pdacid: 4,
     },
   },
 ];
@@ -168,7 +168,7 @@ const RankProd = () => {
     clickTab(index);
   };
   //당도,산미 아이콘
-  const sweetDraw=(score)=>{
+  const sweetDraw = (score) => {
     let i = 0;
     let pdtsweet = [];
     let num = Math.round(score);
@@ -179,6 +179,19 @@ const RankProd = () => {
       pdtsweet.push(<IoWaterOutline />);
     }
     return pdtsweet;
+  }
+  //당도,산미 아이콘
+  const acidDraw = (score) => {
+    let i = 0;
+    let pdacid = [];
+    let num = Math.round(score);
+    for (i = 0; i < num; i++) {
+      pdacid.push(<IoWater />);
+    }
+    for (i = 0; i < 5 - num; i++) {
+      pdacid.push(<IoWaterOutline />);
+    }
+    return pdacid;
   }
 
   return (
@@ -215,26 +228,22 @@ const RankProd = () => {
                 <p>{menuArr[currentTab].content1.pdname}</p>
                 <p className="price">{menuArr[currentTab].content1.pdprice} won</p>
               </div>
-              {menuArr.map((data) => (
-                <div className={styled.tasty_area} key={data.pdtsweet}>
-                  <div className={styled.inner}>
-                    <p>당도</p>
-                    <ul>
-                      {sweetDraw(data.pdtsweet).map((pdtsweet)=>(
-                        <li key={pdtsweet}>{pdtsweet}</li>
-                      ))}
-                    </ul>
-                  </div>
-                  <div className={styled.inner}>
-                    <p>산미</p>
-                    <ul>
-                      {sweetDraw(data.pdtsweet).map((pdtsweet, idx)=>(
-                        <li key={`pdtsweet_${idx}`}>{pdtsweet}</li>
-                      ))}
-                    </ul>
+
+              <div className={styled.tasty_area}>
+                <div className={styled.inner}>
+                  <p>당도</p>
+                  <div>
+                    {sweetDraw(menuArr[currentTab].content1.pdtsweet)}
                   </div>
                 </div>
-              ))}
+                <div className={styled.inner}>
+                  <p>산미</p>
+                  <div>
+                    {acidDraw(menuArr[currentTab].content1.pdacid)}
+                  </div>
+                </div>
+              </div>
+
             </div>
             <ul className={styled.diffrank}>
               <li>
@@ -252,23 +261,15 @@ const RankProd = () => {
                 <div className={styled.tasty_area}>
                   <div className={styled.inner}>
                     <p>당도</p>
-                    <ul>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                    </ul>
+                    <div>
+                      {sweetDraw(menuArr[currentTab].content2.pdtsweet)}
+                    </div>
                   </div>
                   <div className={styled.inner}>
                     <p>산미</p>
-                    <ul>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                    </ul>
+                    <div>
+                      {acidDraw(menuArr[currentTab].content2.pdacid)}
+                    </div>
                   </div>
                 </div>
               </li>
@@ -287,23 +288,15 @@ const RankProd = () => {
                 <div className={styled.tasty_area}>
                   <div className={styled.inner}>
                     <p>당도</p>
-                    <ul>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                    </ul>
+                    <div>
+                      {sweetDraw(menuArr[currentTab].content3.pdtsweet)}
+                    </div>
                   </div>
                   <div className={styled.inner}>
                     <p>산미</p>
-                    <ul>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                    </ul>
+                    <div>
+                      {acidDraw(menuArr[currentTab].content3.pdacid)}
+                    </div>
                   </div>
                 </div>
               </li>
@@ -322,23 +315,15 @@ const RankProd = () => {
                 <div className={styled.tasty_area}>
                   <div className={styled.inner}>
                     <p>당도</p>
-                    <ul>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                    </ul>
+                    <div>
+                      {sweetDraw(menuArr[currentTab].content4.pdtsweet)}
+                    </div>
                   </div>
                   <div className={styled.inner}>
                     <p>산미</p>
-                    <ul>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                    </ul>
+                    <div>
+                      {acidDraw(menuArr[currentTab].content4.pdacid)}
+                    </div>
                   </div>
                 </div>
               </li>
@@ -357,23 +342,15 @@ const RankProd = () => {
                 <div className={styled.tasty_area}>
                   <div className={styled.inner}>
                     <p>당도</p>
-                    <ul>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                    </ul>
+                    <div>
+                      {sweetDraw(menuArr[currentTab].content5.pdtsweet)}
+                    </div>
                   </div>
                   <div className={styled.inner}>
                     <p>산미</p>
-                    <ul>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                      <li><FontAwesomeIcon icon={faDroplet} /></li>
-                    </ul>
+                    <div>
+                      {acidDraw(menuArr[currentTab].content5.pdacid)}
+                    </div>
                   </div>
                 </div>
               </li>
