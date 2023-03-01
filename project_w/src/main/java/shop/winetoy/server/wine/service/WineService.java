ab6f9abf0ea7efd0fafeaa5c7bd37309992aff6b
Hyunsoul37 <soulhyun37@gmail.com>
2023-03-01 23:30:33 +0900

    [FIX] 충돌 오류 수정

--- project_w/src/main/java/shop/winetoy/server/wine/service/WineService.java
+++ project_w/src/main/java/shop/winetoy/server/wine/service/WineService.java
@@ -7,21 +7,11 @@ import shop.winetoy.server.wine.entity.WineInfoDto;
 
 public interface WineService {
 	int registerWine(WineDto registerWine);
-<<<<<<< HEAD
-
 	WineDto getWineDetail(int pid);
-
 	List<WineInfoDto> searchWine(Integer type, Integer body, Integer sweet, Integer acidity, Integer tannin,
 			Integer price, String country, int page);
-
 	List<WineInfoDto> searchWineWithPid(int pid, Integer type, Integer body, Integer sweet, Integer acidity,
 			Integer tannin, Integer price, String country, int page);
-
 	int getWineCount(Integer type, Integer body, Integer sweet, Integer acidity, Integer tannin, Integer price,
 			String country);
-=======
-	WineDto getWineDetail(int pid);
-	List<WineInfoDto> searchWine(Integer type, Integer body, Integer sweet, Integer acidity, Integer tannin, Integer price, String country, int page);
-	List<WineInfoDto> searchWineWithPid(int pid, Integer type, Integer body, Integer sweet, Integer acidity, Integer tannin, Integer price, String country, int page);
->>>>>>> 09c844791dacbd0dcda7bd3305c7fc0f1ff6906c
 }
