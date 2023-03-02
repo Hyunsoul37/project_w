845be62dc7e54ca56ac9c8f1d06ccca0eff451d7
sunmin <dlatjsals123@naver.com>
2023-03-02 21:47:30 +0900

    [ADD] Detail Page 추가

--- project_w/src/main/frontend/src/components/detail/Detail.tsx
+++ project_w/src/main/frontend/src/components/detail/Detail.tsx
@@ -2,9 +2,13 @@ import { wineState } from "../product_sm/productTypes";
 import styled from './Detail.module.css';
 import { FaCircle,FaRegCircle } from "react-icons/fa";
 import { drawStar } from "../util/util";
+import StarRatings from 'react-star-ratings'
 
 const Detail:React.FC<wineState>=(props)=>{
-   
+   const snack = props.wineRecommendedSnack?.split('/');
+   const newarr = snack?.map((data)=>data.replaceAll(" ",""));
+   const arrUnique = [...new Set(newarr)];
+
 return (
 <div className="maxframe">
     <div className={styled.Detail}>
@@ -27,9 +31,15 @@ return (
                         </tr>
                         <tr>
                             <th>평점({props.wineStarPoint}점)</th>
-                            <td></td>
+                            <td><StarRatings 
+                                rating={props.wineStarPoint}
+                                starRatedColor="#61002E"                              
+                                numberOfStars={5}
+                                starDimension="20px"
+                                starSpacing="2px"
+                                name='rating'/></td>
                             <th>알콜도수</th>
-                            <td>{props.wineAlcohol}</td>
+                            <td>{props.wineAlcohol !=='null'? props.wineAlcohol : null}</td>
                         </tr>
                     </tbody>
                 </table>
@@ -54,10 +64,19 @@ return (
             </div>
             <div className={styled.wineFood}>
                 <p>음식매칭</p>
-            </div>
-        </div>      
-        
+                <div>
+                    {arrUnique.map((food)=><img key={food} src={`/images/icon/${food}.PNG`} />)}
+                </div>
+                <div className={styled.wineFoodTextWrapper}>
+                    {arrUnique.map((food)=><div key={`${food}_txt`}>{food}</div>)}
+                </div>             
+            </div>      
+        </div>                  
     </div>   
+    <div className={styled.Desc}>
+        <p>{props.wineKorName}</p>
+        <p>{props.wineDesc}</p>
+    </div>  
 </div>)
 }
 export default Detail;
--- project_w/src/main/frontend/src/components/product_sm/ProductCard.tsx
+++ project_w/src/main/frontend/src/components/product_sm/ProductCard.tsx
@@ -2,14 +2,23 @@ import styled from './ProductCard.module.css';
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 import { useState } from 'react';
 import { wineState } from './productTypes';
+import Link from 'next/link';
 
 const ProductCard: React.FC<wineState> = (props) => {
   const [like, setLike] = useState(props.userLike);
   return (
-
+   
     <div className={styled.ProductCard}>
       <div className={styled.imageWrapper}>
+      <Link href={{
+      pathname:`/product/detail/${props.wineEngName}`,
+      query:{
+        id:props.winePid,
+        winename:props.wineEngName,
+      }
+      }}>
         <img src={`/images/wine/${props.winePid - 1}.png`} />
+      </Link>
         <div className={styled.heart}>
           {like ? (
             <AiFillHeart onClick={() => setLike(false)} />
@@ -18,8 +27,16 @@ const ProductCard: React.FC<wineState> = (props) => {
           )}
         </div>
       </div>
-
+      <Link href={{
+      pathname:`/product/detail/${props.wineEngName}`,
+      query:{
+        id:props.winePid,
+        winename:props.wineEngName,
+      }
+      }}> 
       <p id="name_ko">{props.wineKorName}</p>
+      </Link>
+     
       <p id="name_en">{props.wineEngName}</p>
       <p id="desc">{props.wineDesc}</p>
       <div className={styled.wineInfoBox}>
@@ -29,6 +46,7 @@ const ProductCard: React.FC<wineState> = (props) => {
       </div>
       <h4>{props.winePrice?.replace('원', 'won')}</h4>
     </div>
+    
   );
 };
 export default ProductCard;
