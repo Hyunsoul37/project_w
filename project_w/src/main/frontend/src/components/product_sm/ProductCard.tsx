b8e89bef2b447b51e8ad53df7972e1375d84f54b
sunmin <dlatjsals123@naver.com>
2023-03-04 23:33:52 +0900

    [ADD] Community Page 추가

--- project_w/src/main/frontend/src/components/product_sm/ProductCard.tsx
+++ project_w/src/main/frontend/src/components/product_sm/ProductCard.tsx
@@ -1,42 +1,39 @@
-import styled from './ProductCard.module.css';
-import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
-import { useState } from 'react';
-import { wineState } from './productTypes';
-import Link from 'next/link';
+import styled from "./ProductCard.module.css";
+import { wineState } from "./productTypes";
+import Link from "next/link";
+import Heart from "../util/Heart";
 
 const ProductCard: React.FC<wineState> = (props) => {
-  const [like, setLike] = useState(props.userLike);
   return (
-   
     <div className={styled.ProductCard}>
       <div className={styled.imageWrapper}>
-      <Link href={{
-      pathname:`/product/detail/${props.wineEngName}`,
-      query:{
-        id:props.winePid,
-        winename:props.wineEngName,
-      }
-      }}>
-        <img src={`/images/wine/${props.winePid - 1}.png`} />
-      </Link>
+        <Link
+          href={{
+            pathname: `/product/detail/${props.wineEngName}`,
+            query: {
+              id: props.winePid,
+              winename: props.wineEngName,
+            },
+          }}
+        >
+          <img src={`/images/wine/${props.winePid - 1}.png`} />
+        </Link>
         <div className={styled.heart}>
-          {like ? (
-            <AiFillHeart onClick={() => setLike(false)} />
-          ) : (
-            <AiOutlineHeart onClick={() => setLike(true)} />
-          )}
+          <Heart userLike={props.userLike} />
         </div>
       </div>
-      <Link href={{
-      pathname:`/product/detail/${props.wineEngName}`,
-      query:{
-        id:props.winePid,
-        winename:props.wineEngName,
-      }
-      }}> 
-      <p id="name_ko">{props.wineKorName}</p>
+      <Link
+        href={{
+          pathname: `/product/detail/${props.wineEngName}`,
+          query: {
+            id: props.winePid,
+            winename: props.wineEngName,
+          },
+        }}
+      >
+        <p id="name_ko">{props.wineKorName}</p>
       </Link>
-     
+
       <p id="name_en">{props.wineEngName}</p>
       <p id="desc">{props.wineDesc}</p>
       <div className={styled.wineInfoBox}>
@@ -44,9 +41,8 @@ const ProductCard: React.FC<wineState> = (props) => {
         <span>{props.wineCountry}</span>
         <span>{props.wineGrapeVariety}</span>
       </div>
-      <h4>{props.winePrice?.replace('원', 'won')}</h4>
+      <h4>{props.winePrice?.replace("원", "won")}</h4>
     </div>
-    
   );
 };
 export default ProductCard;
