d9f877e2921973a5d7bbba1fb0d35fa9cb277a78
sunmin <dlatjsals123@naver.com>
2023-03-18 22:57:22 +0900

    [FIX] Button 사이즈 Props 추가

--- project_w/src/main/frontend/src/components/ui/Button.tsx
+++ project_w/src/main/frontend/src/components/ui/Button.tsx
@@ -1,16 +1,29 @@
-import styled from './Button.module.css'
+import styled from "./Button.module.css";
 
-interface buttonProps{
- buttontext: string;
- buttonColor :'main'| 'second';
- onClick?:()=>void;
- onSubmit?: (e:React.FormEvent)=>void | null;
+interface buttonProps {
+  buttontext: string;
+  buttonColor: "main" | "second";
+  buttonSize?: "s" | "m" | "l";
+  onClick?: () => void;
+  onSubmit?: (e: React.FormEvent) => void | null;
 }
 
-const Button:React.FC<buttonProps>=(props)=>{
-    return <button className={props.buttonColor==='main'? styled.main : styled.second} 
-    onClick={props.onClick?props.onClick:undefined}
-    onSubmit={props.onSubmit? props.onSubmit:undefined}
-    >{props.buttontext}</button>
-}
+const Button: React.FC<buttonProps> = (props) => {
+  return (
+    <button
+      className={[
+        props.buttonColor === "main" ? styled.main : styled.second,
+        props.buttonSize === "s"
+          ? styled.small
+          : props.buttonSize === "m"
+          ? styled.medium
+          : styled.large,
+      ].join(" ")}
+      onClick={props.onClick ? props.onClick : undefined}
+      onSubmit={props.onSubmit ? props.onSubmit : undefined}
+    >
+      {props.buttontext}
+    </button>
+  );
+};
 export default Button;
