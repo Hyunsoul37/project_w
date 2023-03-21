91c68b8823fb669e78a533c9d62379a5f52bb11b
sunmin <dlatjsals123@naver.com>
2023-03-22 01:05:33 +0900

    [FIX] 푸터 수정중

--- project_w/src/main/frontend/src/components/home/Footer.js
+++ project_w/src/main/frontend/src/components/home/Footer.js
@@ -1,6 +1,25 @@
 import styled from "./Footer.module.css";
-
+import { AiFillGithub } from "react-icons/ai";
+//AiFillGithub
 const Footer = () => {
-  return <div className={styled.Footer}> 푸터</div>;
+  return (
+    <footer className={styled.Footer}>
+      <div className={styled.devInfo}>
+        <div>
+          <h1>이호현</h1>
+          <AiFillGithub />
+        </div>
+        <div>
+          <h1>임선민</h1>
+          <AiFillGithub />
+        </div>
+        <div>
+          <h1>신효진</h1>
+          <AiFillGithub />
+        </div>
+      </div>
+      <p>본 사이트는 포트폴리오용 사이트입니다.</p>
+    </footer>
+  );
 };
 export default Footer;
