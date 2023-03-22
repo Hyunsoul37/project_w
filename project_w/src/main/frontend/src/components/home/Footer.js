1bc2efe92c96138931a5a12e44ee26d53d2bd0bd
sunmin <dlatjsals123@naver.com>
2023-03-22 21:59:20 +0900

    [FIX] Footer 내용 수정

--- project_w/src/main/frontend/src/components/home/Footer.js
+++ project_w/src/main/frontend/src/components/home/Footer.js
@@ -4,21 +4,42 @@ import { AiFillGithub } from "react-icons/ai";
 const Footer = () => {
   return (
     <footer className={styled.Footer}>
-      <div className={styled.devInfo}>
-        <div>
-          <h1>이호현</h1>
-          <AiFillGithub />
-        </div>
-        <div>
-          <h1>임선민</h1>
-          <AiFillGithub />
-        </div>
-        <div>
-          <h1>신효진</h1>
-          <AiFillGithub />
+      <div className={styled.Footer_Info}>
+        <div className={styled.logo}>WAGU</div>
+        <div className={styled.devInfo}>
+          <div>
+            <h1>BackEnd</h1>
+            <h3>Lee ho hyun</h3>
+            <p>
+              <span style={{ marginRight: "10px" }}>E-MAIL</span>{" "}
+              soulhyun37@gmail.com
+            </p>
+            <AiFillGithub />
+          </div>
+          <div>
+            <h1>FrontEnd</h1>
+            <h3>Lim sun min</h3>
+            <p>
+              <span style={{ marginRight: "10px" }}>E-MAIL</span>{" "}
+              dlatjsals123@naver.com
+            </p>
+            <AiFillGithub />
+          </div>
+          <div>
+            <h1>Designer</h1>
+            <h3>Shin hyo jin</h3>
+            <p>
+              <span style={{ marginRight: "10px" }}>E-MAIL</span>{" "}
+              tlsgywls1107@naver.com
+            </p>
+            <AiFillGithub />
+          </div>
         </div>
       </div>
-      <p>본 사이트는 포트폴리오용 사이트입니다.</p>
+
+      <p>
+        본 사이트는 상업적 목적이 아닌 포트폴리오용으로 제작된 사이트입니다.
+      </p>
     </footer>
   );
 };
