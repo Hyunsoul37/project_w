8d57e48e724f21e729ea29d3567258954e9b8dd9
sunmin <dlatjsals123@naver.com>
2023-03-25 02:29:53 +0900

    [ADD] wineSlice 추가

---
+++ project_w/src/main/frontend/src/slice/wineSlice.js
@@ -0,0 +1,86 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  isLoadding: true,
+  isSuccess: false,
+  ListErrormsg: "",
+  CountErrormsg: "",
+  pageNum: -1,
+  data: {
+    pid: -1,
+    korName: "",
+    engName: "",
+    price: 0,
+    type: 1,
+    body: 1,
+    acidity: 1,
+    tannin: 1,
+    sweet: 1,
+    alcohol: "",
+    country: "",
+    region: "",
+    desc: "",
+    starPoint: 0,
+    recommendedSnack: "",
+    grapeVariety: "",
+    like: false,
+  },
+};
+
+export const GetList = createAsyncThunk("wine/GetList", async (data) => {
+  console.log(data);
+  const response = await fetch(
+    `/api/product/wine/${data.userId !== -1 ? data.userId : 0}/search${
+      data.queryurl
+    }`,
+    {
+      method: "GET",
+      body: null,
+      headers: { "Content-Type": "application/json;charset=UTF-8" },
+    }
+  );
+  const responseData = await response.json();
+  return responseData;
+});
+
+export const GetCount = createAsyncThunk("wine/GetCount", async (data) => {
+  const response = await fetch(
+    `/api/product/wine/count${data.counturl}
+    `,
+    {
+      method: "GET",
+      body: null,
+      headers: { "Content-Type": "application/json;charset=UTF-8" },
+    }
+  );
+  const responseData = await response.json();
+  return responseData;
+});
+
+export const wineSlice = createSlice({
+  name: "wine",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(GetList.pending, (state, action) => {
+        state.isLoadding = true;
+      })
+      .addCase(GetList.fulfilled, (state, action) => {
+        state.isLoadding = false;
+        state.isSuccess = true;
+        state.data = action.payload.data;
+      })
+      .addCase(GetList.rejected, (state, action) => {
+        state.isSuccess = false;
+        state.ListErrormsg = action.error;
+      });
+
+    builder
+      .addCase(GetCount.fulfilled, (state, action) => {
+        state.pageNum = action.payload.data;
+      })
+      .addCase(GetCount.rejected, (state, action) => {
+        state.CountErrormsg = action.error;
+      });
+  },
+});
--- project_w/src/main/frontend/src/store.ts
+++ project_w/src/main/frontend/src/store.ts
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { reviewSlice } from "./slice/reviewSlice";
 import { userSlice } from "./slice/userSlice";
+import { wineSlice } from "./slice/wineSlice";
 
 export const store = configureStore({
-  reducer: { user: userSlice.reducer, review: reviewSlice.reducer },
+  reducer: {
+    user: userSlice.reducer,
+    review: reviewSlice.reducer,
+    wine: wineSlice.reducer,
+  },
   // devTools: true,
   devTools: process.env.NODE_ENV !== "production",
 });
