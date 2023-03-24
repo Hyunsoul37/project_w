e8e22e62b33abad1879d3d743363e89d22b07504
sunmin <dlatjsals123@naver.com>
2023-03-25 02:28:58 +0900

    [FIX] 이미지 확장자 avif 제외

--- project_w/src/main/frontend/next.config.js
+++ project_w/src/main/frontend/next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
         hostname: "winetoy.s3.ap-northeast-2.amazonaws.com",
       },
     ],
-    formats: ["image/avif", "image/webp"],
+    formats: ["image/webp"],
   },
   async rewrites() {
     return [
