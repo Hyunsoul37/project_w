675efe71485b802cf423f04e3e39ab90a6c8bf22
sunmin <dlatjsals123@naver.com>
2023-04-03 01:20:25 +0900

    [FIX] detail Page 데이터 로드 부분 수정

--- project_w/src/main/frontend/src/pages/product/detail/[...params].js
+++ project_w/src/main/frontend/src/pages/product/detail/[...params].js
@@ -1,29 +1,21 @@
-import data from "../../../components/dummydata/wine_add_sweet.json";
+import axios from "axios";
 import Detail from "../../../components/detail/Detail.tsx";
 import WineTerms from "../../../components/detail/Wineterms";
 import Seo from "../../../util/Seo";
 import { useEffect, useState } from "react";
 
-const detailPage = ({ id }) => {
-  const [detailData, setDetailData] = useState({});
+const detailPage = ({ data }) => {
+  //const [detailData, setDetailData] = useState({});
   //const filterlist = data.filter((d) => d.winePid === Number(id));
-  const GetDetailInfo = async () => {
-    const response = await fetch(`/api/product/wine/detail?pid=${id}`, {
-      method: "GET",
-      body: null,
-      headers: { "Content-Type": "application/json;charset=UTF-8" },
-    });
-    const data = await response.json();
-    setDetailData(data.data);
-  };
-  useEffect(() => {
-    GetDetailInfo();
-  }, []);
+
+  // useEffect(() => {
+  //   GetDetailInfo();
+  // }, []);
 
   return (
     <>
-      <Seo title={detailData.wineKorName} />
-      {detailData !== undefined ? <Detail {...detailData} /> : <></>}
+      <Seo title={data.wineKorName} />
+      <Detail {...data} />
       <WineTerms />
     </>
   );
@@ -31,10 +23,21 @@ const detailPage = ({ id }) => {
 export default detailPage;
 
 export async function getServerSideProps({ query }) {
-  //여기에 서버에서 받는 코드 작성
   const { id } = query;
-
+  let data = {};
+  await axios
+    .get(`http://winetoy.shop/api/product/wine/detail?pid=${id}`)
+    .then((res) => (data = res.data.data));
+  // const GetDetailInfo = async () => {
+  //   const response = await fetch(`/api/product/wine/detail?pid=${id}`, {
+  //     method: "GET",
+  //     body: null,
+  //     headers: { "Content-Type": "application/json;charset=UTF-8" },
+  //   });
+  //   const data = await response.json();
+  //   setDetailData(data.data);
+  // };
   return {
-    props: { id },
+    props: { data },
   };
 }
