c99f8246da7ec40c532d6fd9dd31d3c8ae6a5536
sunmin <dlatjsals123@naver.com>
2023-04-27 13:35:19 +0900

    [ADD] google Search Console 소유권 확인을 위한 meta태그 추가

--- project_w/src/main/frontend/src/pages/_document.js
+++ project_w/src/main/frontend/src/pages/_document.js
@@ -1,12 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-        <link
-          href="/font/font.css"
-          rel="stylesheet"
+        <link href="/font/font.css" rel="stylesheet" />
+        <meta
+          name="google-site-verification"
+          content="HC_-ZbJtY9NQHbgdTdF_Fz17JG1FcfR3YEm4kNTivdw"
         />
       </Head>
 
